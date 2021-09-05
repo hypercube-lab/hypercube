188dfecea4725ce5c31185586159395396c04823
agnitazudkll <89609148+agnitazudkll@users.noreply.github.com>
2021-09-05 12:53:06 +0800

    Update update.py

--- src/update.py
+++ src/update.py
@@ -16,6 +16,25 @@ def modify():
         file.close()
 
 
+def loopfile():
+    directory = os.getcwd()
+    for filename in os.listdir(directory):
+        if filename.endswith(".rs") or filename.endswith(".png"):
+            print(os.path.join(directory, filename))
+
+            with open(filename, "a+") as file_object:
+                    # Move read cursor to the start of file.
+                    file_object.seek(0)
+                    # If file is not empty then append '\n'
+                    data = file_object.read(100)
+                    if len(data) > 0 :
+                        file_object.write("\n")
+                    # Append text at the end of file
+                    #file_object.write("hello hi")
+                    
+        else:
+            continue
+
 def commit():
     os.system('git commit -a -m "merge and update" > /dev/null 2>&1')
 
