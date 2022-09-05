5c2e9e31143b26e3aeda564f85b709416d170519
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-05 10:37:56 +0800

    Delete programs directory

--- programs/dummy/src/lib.rs
+++
@@ -1,6 +0,0 @@
-extern crate xpz_program_interface;
-
-use xpz_program_interface::account::KeyedAccount;
-
-#[no_mangle]
-pub extern "C" fn process(_infos: &mut Vec<KeyedAccount>, _data: &[u8]) {}
--- programs/print/src/lib.rs
+++
@@ -1,9 +0,0 @@
-extern crate xpz_program_interface;
-
-use xpz_program_interface::account::KeyedAccount;
-
-#[no_mangle]
-pub extern "C" fn process(infos: &mut Vec<KeyedAccount>, _data: &[u8]) {
-    println!("AccountInfos: {:#?}", infos);
-    //println!("data: {:#?}", data);
-}
